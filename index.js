a73ce855bf47e620f149591f8a37e3365eda28db
rahul mahato <rahulmahato0852@gmail.com>
2024-02-15 11:30:54 +0530

    deleted

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const express = require("express");
-const cors = require("cors");
+var mongoose = require("mongoose");
+var express = require("express");
+var cors = require("cors");
 require("dotenv").config({ path: "./.env" });
-class CustomError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'CustomError';
+var CustomError = /** @class */ (function (_super) {
+    __extends(CustomError, _super);
+    function CustomError(message) {
+        var _this = _super.call(this, message) || this;
+        _this.name = 'CustomError';
+        return _this;
     }
-}
+    return CustomError;
+}(Error));
 mongoose.connect(process.env.MONGO);
-const app = express();
+var app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/portfolio", require("./routes/userRoute"));
-app.use("*", (req, res) => {
+app.use("*", function (req, res) {
     res.status(404).json({ message: "No Resource Found" });
 });
-app.use((err, req, res, next) => {
+app.use(function (err, req, res, next) {
     res.status(500).json({ message: err.message });
 });
-mongoose.connection.once("open", () => {
+mongoose.connection.once("open", function () {
     console.log("Mongoose connected");
     app.listen(process.env.PORT, console.log("server running"));
 });
---
+++ utils/sendEmail.ts
@@ -0,0 +1,41 @@
+import nodemailer from "nodemailer"
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config({ path: "./.env" });
+
+interface EmailOptions {
+    to?: string;
+    subject: string;
+    text: string;
+    html?: string
+}
+
+const sendEmail = (emailOptions: EmailOptions): Promise<any> => new Promise((resolve, reject) => {
+    try {
+        const { to = process.env.FROM_EMAIL, subject = "Inquary", text = "FOR HIRING", html = `` } = emailOptions
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.FROM_EMAIL,
+                pass: process.env.EMAIL_PASS
+            }
+        })
+
+        transporter.sendMail({ to, from: process.env.FROM_EMAIL, subject: subject, text: text, html }, (error) => {
+            if (error) {
+                console.log(error)
+                return reject(error.message)
+            }
+            resolve("Email Send Success")
+
+        })
+
+
+    } catch (error: any) {
+        return reject(error.message)
+    }
+})
+
+
+export default sendEmail
