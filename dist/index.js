c4c7fa0b8439c2f24407f605002430da47c8ed3e
Rahul <rahulmahato0852@gmail.com>
2025-08-27 15:47:54 +0530

    fix TS version

--- dist/index.js
+++ dist/index.js
@@ -5,14 +5,9 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config({ path: "./.env" });
-class CustomError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'CustomError';
-    }
-}
 mongoose.connect(process.env.MONGO);
 const app = express();
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cors());
@@ -21,8 +16,9 @@ app.use(express.static("projects"));
 app.use("/api/portfolio", require("./routes/userRoute"));
 app.use("/api/admin", require("./routes/adminRoute"));
 app.use("*", (req, res) => {
+    // res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
     res.sendFile(path.join(__dirname, "public", "index.html"));
-    // res.status(404).json({ message: "No Resource Found" })
+    res.status(404).json({ message: "No Resource Found" });
 });
 app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
